de0999212a7465b0113dd3c0669e5a79e0c48543
Tyler Pritchard <pritchard.tyler@gmail.com>
2018-12-07 12:00:04 -0800

    trying to get images to render

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const port = process.env.PORT || 8000
-// app.set( 'port', ( process.env.PORT || 5000 ));
 const app = express()
+const cors = require('cors');
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
-const cors = require('cors');
+app.use(express.static('public'))
+app.use(morgan('dev'));
 app.use(cors());
 
 /**
@@ -12,10 +14,10 @@ app.use(cors());
  */
 
 const menuItemRoutes = require('./src/routes/routes.menuItems');
-const userRoutes = require('./src/routes/routes.users');
-const typeRoutes = require('./src/routes/routes.types');
-// const orderRoutes = require('./src/routes/orders');
-const menuItemOrderRoutes = require('./src/routes/routes.menuItemOrders');
+// const userRoutes = require('./src/routes/routes.users');
+// const typeRoutes = require('./src/routes/routes.types');
+// // const orderRoutes = require('./src/routes/orders');
+// const menuItemOrderRoutes = require('./src/routes/routes.menuItemOrders');
 
 
 
@@ -25,25 +27,19 @@ const menuItemOrderRoutes = require('./src/routes/routes.menuItemOrders');
  */
 
 app.use('/menuItems', menuItemRoutes);
-app.use('/users', userRoutes);
-app.use('/types', typeRoutes);
 // app.use('/orders', orderRoutes);
-app.use('/menuItemOrders', menuItemOrderRoutes);
 
-
-
- /**
-  * Open up port for application to be accessed
-  */
+/**
+ * Open up port for application to be accessed
+ */
 
 app.listen(port, console.log(`successfully running backend on ${port}`))
 // app.listen(process.env.PORT || 8000)
 // app.listen( app.get( 'port' ), function() {
 //   console.log( 'Node server is running on port ' + app.get( 'port' ));
 //   });
-  
-  //DELETE table for 'menuItems'
-  exports.down = function(knex, Promise) {
-    return knex.schema.dropTable
-    ('menuItems')
-  };
+
+//DELETE table for 'menuItems'
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('menuItems')
+};
--- seeds/03_menuItems.js
+++ seeds/03_menuItems.js
@@ -1,31 +1,29 @@
-
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
   return knex('menuItems')
     .del()
     .then(() => {
       // Inserts seed entries
-      return knex('menuItems').insert([
-        {
+      return knex('menuItems').insert([{
           name: 'Breakfast Sandwich',
           price: 0650,
           description: 'Egg, bacon, cheddar cheese on english muffin',
           type_id: 1,
-          photo_url: '/IMG_0544.jpg'
+          photo_url: '../public/images/breakfastsandwich.jpg'
         },
         {
           name: 'B.L.T',
           price: 1100,
           description: 'Bacon, greens, tomato jam, spicy aioli on toasted wheat bread',
           type_id: 2,
-          photo_url: '/IMG_0544.jpg'
+          photo_url: '../public/images/blt.jpg'
         },
         {
           name: 'Mixed Green Salad (v)',
           price: 0250,
           description: 'Original/Cilantro Lime/Orange balsamic',
           type_id: 3,
-          photo_url: '/IMG_0544.jpg'
+          photo_url: '../public/images/salad.jpg'
         }
       ])
     })
