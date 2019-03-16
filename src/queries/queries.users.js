90ed017a086fa7358e463a7b5b05876a8c8b3518
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-03-15 17:59:50 -0700

    delete comments, formatting queries.users

--- src/queries/queries.users.js
+++ src/queries/queries.users.js
@@ -1,11 +1,9 @@
-//Access 'users' table
 const knex = require('./db')
 
 getAllUsers = () => {
     return knex('users')
 }
 
-//allows other modules to utilize the 'getAllusers' function
 module.exports = {
     getAllUsers
 }
