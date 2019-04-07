42e11d146eefcd5a73389453b9205799370d058b
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-04-06 17:47:05 -0700

    remove comments from 01_users.js

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -1,34 +1,29 @@
+exports.seed = function (knex, Promise) {
+  return knex('users')
+    .del()
+    .then(() => {
+      return knex('users').insert([{
+          id: 1,
+          first_name: 'Nick',
+          last_name: 'Jonas',
+          email: 'nickjonas@purityrings.com',
+          password: 'password'
+        },
+        {
+          id: 2,
+          first_name: 'Joe',
+          last_name: 'Jonas',
+          email: 'joejonas@purityrings.com',
+          password: 'password'
 
-exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex('users')
-      .del()
-      .then(() => {
-        // Inserts seed entries
-        return knex('users').insert([
-          {
-            id: 1,
-            first_name: 'Nick',
-            last_name: 'Jonas',
-            email: 'nickjonas@purityrings.com',
-            password: 'password'
-          },
-          {
-            id: 2,
-            first_name: 'Joe',
-            last_name: 'Jonas',
-            email: 'joejonas@purityrings.com',
-            password: 'password'
-
-          },
-          {
-            id: 3,
-            first_name: 'Kevin',
-            last_name: 'Jonas',
-            email: 'kevinjonas@purityrings.com',
-            password: 'password'
-          }
-        ])
-      })
-  }
-  
+        },
+        {
+          id: 3,
+          first_name: 'Kevin',
+          last_name: 'Jonas',
+          email: 'kevinjonas@purityrings.com',
+          password: 'password'
+        }
+      ])
+    })
+}
