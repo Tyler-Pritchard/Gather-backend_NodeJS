e5479da154b328aafba72e2a786ad3b79ed2d372
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-04-06 17:49:52 -0700

    remove comments 06_menuItems

--- seeds/06_menuItems_types.js
+++ seeds/06_menuItems_types.js
@@ -1,24 +1,19 @@
-
-exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex('menuItems_types')
-      .del()
-      .then(() => {
-        // Inserts seed entries
-        return knex('menuItems_types').insert([
-          {
-            menuItems_id: 1,
-            types_id: 1
-          },
-          {
-            menuItems_id: 2,
-            types_id: 2
-          },
-          {
-            menuItems_id: 3,
-            types_id: 3
-          }
-        ])
-      })
-  }
-  
+exports.seed = function (knex, Promise) {
+  return knex('menuItems_types')
+    .del()
+    .then(() => {
+      return knex('menuItems_types').insert([{
+          menuItems_id: 1,
+          types_id: 1
+        },
+        {
+          menuItems_id: 2,
+          types_id: 2
+        },
+        {
+          menuItems_id: 3,
+          types_id: 3
+        }
+      ])
+    })
+}
